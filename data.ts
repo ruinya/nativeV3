1de7f62265e55a89c31070c4e9c3cd26cc854b74
ruinya <94568765+ruinya@users.noreply.github.com>
2024-12-10 00:55:58 +0300

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,22 @@
+import { ProgrammingLanguage } from "./src/types/ProgrammingLanguage";
+
+export const programmingLanguages: ProgrammingLanguage[] = [
+  {
+    id: 1,
+    name: "JavaScript",
+    experience: "3 years",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    id: 2,
+    name: "Python",
+    experience: "1 years",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+  },
+  {
+    id: 3,
+    name: "Java",
+    experience: "1 year",
+    logo: "https://upload.wikimedia.org/wikipedia/ru/thumb/3/39/Java_logo.svg/800px-Java_logo.svg.png",
+  },
+];
